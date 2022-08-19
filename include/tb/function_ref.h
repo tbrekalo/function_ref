83e31c7e77b1b09d6b944dd01a6a859c242ad3d1
tbrekalo <brekalo.tvrtko@gmail.com>
2022-08-19 20:10:42 +0200

    tests and bug fixes

--- include/tb/function_ref.h
+++ include/tb/function_ref.h
@@ -1,7 +1,6 @@
 #ifndef TB_FUNCTION_REF_
 #define TB_FUNCTION_REF_
 
-#include <functional>
 #include <memory>
 #include <type_traits>
 
@@ -12,26 +11,51 @@ struct function_ref_traits;
 
 template <class R, class... Args>
 struct function_ref_traits<R(Args...)> {
-  static auto constexpr is_noexcept = false;
+  static constexpr auto is_noexcept = false;
 
-  using signature = R(Args...);
+  template <class T>
+  using cv = T;
 
   template <class Fn>
   static constexpr auto is_compatible_v = std::is_invocable_r_v<R, Fn, Args...>;
+};
+
+template <class R, class... Args>
+struct function_ref_traits<R(Args...) noexcept> {
+  static constexpr auto is_noexcept = true;
+
+  template <class T>
+  using cv = T;
+
+  template <class Fn>
+  static constexpr auto is_compatible_v =
+      std::is_nothrow_invocable_r_v<R, Fn, Args...>;
+};
+
+template <class R, class... Args>
+struct function_ref_traits<R(Args...) const> : function_ref_traits<R(Args...)> {
+  template <class T>
+  using cv = T const;
+};
+
+template <class R, class... Args>
+struct function_ref_traits<R(Args...) const noexcept>
+    : function_ref_traits<R(Args...) noexcept> {
+  template <class T>
+  using cv = T const;
 };
 
 struct function_ref_base {
   union storage_t {
     constexpr storage_t() noexcept = default;
 
-    template <class T, class = std::enable_if_t<std::is_object_v<T>>>
+    template <class T, std::enable_if_t<std::is_object_v<T>, int> = 0>
     constexpr storage_t(T* t) noexcept : obj_ptr_(t) {}
 
-    template <class T, class = std::enable_if_t<std::is_object_v<T>>>
-    constexpr storage_t(T const* t) noexcept : obj_ptr_const_(t) {}
+    template <class T, std::enable_if_t<std::is_const_v<T>, int> = 1>
+    constexpr storage_t(T* t) noexcept : obj_ptr_const_(t) {}
 
-    template <class T,
-              std::enable_if_t<std::is_function_v<T>, std::nullptr_t> = nullptr>
+    template <class T, std::enable_if_t<std::is_function_v<T>, int> = 2>
     constexpr storage_t(T* t) noexcept
         : fun_ptr_(reinterpret_cast<void (*)()>(t)) {}
 
@@ -43,11 +67,11 @@ struct function_ref_base {
   template <class T>
   static constexpr auto get(storage_t storage) noexcept {
     if constexpr (std::is_function_v<T>) {
-      return storage.fun_ptr_;
-    } else if (std::is_const_v<T>) {
-      return storage.obj_ptr_const_;
-    } else if (std::is_object_v<T>) {
-      return storage.obj_ptr_;
+      return reinterpret_cast<T*>(storage.fun_ptr_);
+    } else if constexpr (std::is_const_v<T>) {
+      return static_cast<T*>(storage.obj_ptr_const_);
+    } else if constexpr (std::is_object_v<T>) {
+      return static_cast<T*>(storage.obj_ptr_);
     }
   }
 };
@@ -59,34 +83,66 @@ namespace tb {
 template <class Signature>
 class function_ref;
 
-template <class R, class... Args>
-class function_ref<R(Args...)> : detail::function_ref_base {
-  using traits = detail::function_ref_traits<R(Args...)>;
+#define FUN_REF(...)                                                           \
+  template <class R, class... Args>                                            \
+  class function_ref<R(Args...) __VA_ARGS__> : detail::function_ref_base {     \
+    using traits = detail::function_ref_traits<R(Args...) __VA_ARGS__>;        \
+                                                                               \
+    template <class T>                                                         \
+    using ref_t = typename traits::template cv<T>&;                            \
+                                                                               \
+    using invoker_t = R(storage_t, Args...);                                   \
+    using invoker_ptr_t = std::add_pointer_t<invoker_t>;                       \
+                                                                               \
+   public:                                                                     \
+    template <class F, class = std::enable_if_t<                               \
+                           traits::template is_compatible_v<F> and             \
+                           not std::is_member_function_pointer_v<F> and        \
+                           std::is_function_v<F>>>                             \
+    explicit constexpr function_ref(F* fun_ptr) noexcept                       \
+        : storage_(fun_ptr),                                                   \
+          invoker_ptr_([](storage_t storage,                                   \
+                          Args... args) noexcept(traits::is_noexcept) -> R {   \
+            if constexpr (std::is_same_v<R, void>) {                           \
+              get<F>(storage)(std::forward<Args>(args)...);                    \
+            } else {                                                           \
+              return get<F>(storage)(std::forward<Args>(args)...);             \
+            }                                                                  \
+          }) {}                                                                \
+                                                                               \
+    template <class F,                                                         \
+              class = std::enable_if_t<traits::template is_compatible_v<F> and \
+                                       std::is_object_v<F> and                 \
+                                       not std::is_same_v<function_ref, F>>>   \
+    explicit constexpr function_ref(F& fun_obj) noexcept                       \
+        : storage_(std::addressof(fun_obj)),                                   \
+          invoker_ptr_([](storage_t storage,                                   \
+                          Args... args) noexcept(traits::is_noexcept) -> R {   \
+            ref_t<F> ref = *get<F>(storage);                                   \
+            if constexpr (std::is_same_v<R, void>) {                           \
+              ref(std::forward<Args>(args)...);                                \
+            } else {                                                           \
+              return ref(std::forward<Args>(args)...);                         \
+            }                                                                  \
+          }) {}                                                                \
+                                                                               \
+    constexpr auto operator()(Args... args) const                              \
+        noexcept(traits::is_noexcept) -> R {                                   \
+      return invoker_ptr_(storage_, std::forward<Args>(args)...);              \
+    }                                                                          \
+                                                                               \
+   private:                                                                    \
+    storage_t storage_;                                                        \
+    invoker_ptr_t invoker_ptr_;                                                \
+  };
 
-  using invoker_t = R(storage_t, Args...);
-  using invoker_ptr_t = std::add_pointer_t<invoker_t>;
+FUN_REF()
+FUN_REF(noexcept)
 
- public:
-  template <class F,
-            class = std::enable_if_t<traits::template is_compatible_v<F> and
-                                     std::is_function_v<F>>>
-  explicit constexpr function_ref(F* fun_ptr)
-      : storage_(fun_ptr),
-        invoker_ptr_([](storage_t storage,
-                        Args... args) noexcept(traits::is_noexcept) -> R {
-          return std::invoke(reinterpret_cast<F*>(get<F>(storage)),
-                             std::forward<Args>(args)...);
-        }) {}
+FUN_REF(const)
+FUN_REF(const noexcept)
 
-  constexpr auto operator()(Args... args) const noexcept(traits::is_noexcept)
-      -> R {
-    return invoker_ptr_(storage_, std::forward<Args>(args)...);
-  }
-
- private:
-  storage_t storage_;
-  invoker_ptr_t invoker_ptr_;
-};
+#undef FUN_REF
 
 }  // namespace tb
 
--- test/main.cc
+++ test/main.cc
@@ -1,11 +1,38 @@
-#include "tb/function_ref.h"
 #include "catch2/catch_test_macros.hpp"
+#include "tb/function_ref.h"
 
-static auto foo() -> int {
-  return 42;
-}
+static constexpr auto kIntConstant = 42;
 
-TEST_CASE("play") {
+static auto foo() -> int { return kIntConstant; }
+static auto bar(int& x) -> void { x += kIntConstant; }
+
+TEST_CASE("int()_from_ptr") {
   auto foo_ref = tb::function_ref<int()>(&foo);
   CHECK(foo_ref() == foo());
+}
+
+TEST_CASE("int()_from_ref") {
+  auto foo_ref = tb::function_ref<int()>(foo);
+  CHECK(foo_ref() == foo());
+}
+
+TEST_CASE("void(int&)_from_ptr") {
+  auto bar_ref = tb::function_ref<void(int&)>(&bar);
+  auto x = 0;
+
+  bar_ref(x);
+  CHECK(x == kIntConstant);
+}
+
+TEST_CASE("void(int&)_from_ref") {
+  auto bar_ref = tb::function_ref<void(int&)>(bar);
+  auto x = 0;
+
+  bar_ref(x);
+  CHECK(x == kIntConstant);
+}
+
+TEST_CASE("int()_from_lambda") {
+  // auto lambda = [k = kIntConstant]() -> int { return k; };
+  // tb::function_ref<int() const noexcept> lambda_ref(lambda);
 }
